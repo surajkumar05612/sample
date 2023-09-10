fb27c27f17ca55fa5f14a449e3353fc8cfd4e19a
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2023-09-11 00:09:55 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connectDB = async()=> {
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect("mongodb+srv://root:rTCVgNup3YlvRFzS@cluster0.4g83if7.mongodb.net/?retryWrites=true&w=majority");
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
